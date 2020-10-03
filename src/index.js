346f5f4fa6f07f83f2982a8f50503a942a7b4e1c
Nadzeya Kuranava <nadya-venera@yandex.ru>
2020-10-03 23:38:27 +0300

    Commit message

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+const returnZeroIfEmpty = (array, callback) => {
+   if (typeof array === 'undefined' || array.length === 0) {
+      return 0; 
+    }
+     return callback(array);
+    };
+    
+const min = (array) => {
+   return returnZeroIfEmpty(array, () => {
+      let min = array[0]; // assuming the lowest number is the first element 
+      for (let i = 1; i < array.length; i++) {
+         if (array[i] < min) {
+            // if the current element is lower than min, let min be current element
+       min = array[i];
+       }
+       }
+        return min; 
+      });
+    };
+    
+    
+  const max = (array) => {
+     return returnZeroIfEmpty(array, () => {
+        let max = array[0]; // assuming the biggest number is the first element
+         for (let i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+               // if the current element is bigger than max, let max be current element 
+      max = array[i];
+     }
+     } return max; 
+    });
+  }; 
+  
+  
+  const avg = (array) => {
+    
+  return returnZeroIfEmpty(array, () => { 
+    let sum = 0;
+     for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+       }
+        return sum / array.length;
+       });
+      }; 
+      exports.min = min;
+      exports.max = max;
+      exports.avg = avg;
